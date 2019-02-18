019b92244d12f62c16341c0f1ac9cecda43a5348
Nicholas Morrow <njmorrow95@gmail.com>
2019-02-18 14:17:13 -0500

    version

--- Web/Controllers/TestController.cs
+++ Web/Controllers/TestController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Web.Models;
 using Web.Services;
 using WebApplication.Models;
 
@@ -31,21 +30,11 @@ namespace WebApplication.Controllers
         {
             return this._context.TestValues.Select(tv => tv.TestValueId).ToList();
         }
-        
-        [HttpPost("[action]/{testValueId}")]
-        public IEnumerable<string> AddPersistedValue(string testValueId)
-        { 
-            this._context.TestValues.Add(new TestValue { TestValueId = testValueId});
-            this._context.SaveChanges();
-            return this._context.TestValues.Select(tv => tv.TestValueId).ToList();
-        }
 
-        [HttpPost("[action]")]
-        public IEnumerable<string> AddPersistedValue2([FromBody] TestValue testValue)
+        [HttpGet("[action]")]
+        public int GetVersion()
         {
-            this._context.TestValues.Add(testValue);
-            this._context.SaveChanges();
-            return this._context.TestValues.Select(tv => tv.TestValueId).ToList();
+            return 3;
         }
     }
 }
